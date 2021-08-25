84378689f4042560f0557c0760771f7c782cc9fa
Orrrra <orexxx10@gmail.com>
2021-08-25 14:50:26 +0300

    main

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,4 @@
-@@include('alert.js')
-
+'use strict'
 
 // function testWebP(callback) {
 
@@ -10,19 +9,26 @@
 // 	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 // }
 
-testWebP(function (support) {
+// testWebP(function (support) {
 
-	if (support == true) {
-		document.querySelector('body').classList.add('webp');
-	} else {
-		document.querySelector('body').classList.add('no-webp');
-	}
-});
+// 	if (support == true) {
+// 		document.querySelector('body').classList.add('webp');
+// 	} else {
+// 		document.querySelector('body').classList.add('no-webp');
+// 	}
+// });
 
+
+// $(function () {
+// 	$('.menu__icon').click(function (event) {
+// 		$('.menu__icon, .menu__body').toggleClass('_active');
+// 		$('body').toggleClass('lock');
+// 	});
+// });
 
 $(function () {
-	$('.menu__icon').click(function (event) {
-		$('.menu__icon, .menu__body').toggleClass('_active');
+	$('.header__burger').click(function () {
+		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 });
